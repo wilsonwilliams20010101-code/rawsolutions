357e345fecb9bc8438051ca974d3e369689cea14
wilsonwilliams20010101-code <wilsonwilliams20010101@gmail.com>
2026-09-22 17:32:23 +0530

    Prepare Express server for Render

--- app.js
+++ app.js
@@ -2,20 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.get('/', (req, res) => res.render('index'));
 
 const PORT = process.env.PORT || 3000;
-
-console.log('GoDaddy PORT:', process.env.PORT);
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`RAW Solutions running on port ${PORT}`);
 });
